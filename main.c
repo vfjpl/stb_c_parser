01644ad0eda6e845fdbad407399cc29fe096dc21
vfjpl <vfjpl1@gmail.com>
2025-07-06 15:49:48 +0200

    working :)

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "stb_c_lexer.h"
 #include "utils.h"
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-static FILE* cpreprocessed_fopen(const char* filename)
+static FILE* preprocessed_fopen(const char* filename)
 {
 	const char* tmp_filename = tmpnam(NULL);
 	string_t str = util_asprintf("cpp %s > %s", filename, tmp_filename);
@@ -14,10 +15,10 @@ static FILE* cpreprocessed_fopen(const char* filename)
 	return file;
 }
 
-static string_t cpreprocess_file(const char* filename)
+static string_t preprocess_file(const char* filename)
 {
 	string_t str = {0};
-	FILE* file = cpreprocessed_fopen(filename);
+	FILE* file = preprocessed_fopen(filename);
 	if(!file) return str;
 	fseek(file, 0, SEEK_END);
 	str.size = ftell(file);
@@ -29,9 +30,9 @@ static string_t cpreprocess_file(const char* filename)
 	return str;
 }
 
-static void print_tokens(const char* filename)
+static void print_tokens(const char* filename, bool preprocess)
 {
-	string_t str = cpreprocess_file(filename);
+	string_t str = preprocess ? preprocess_file(filename) : util_readfile(filename);
 
 	char buff[4096] = {0};
 	stb_lexer lexer = {0};
@@ -41,7 +42,7 @@ static void print_tokens(const char* filename)
 	while(stb_c_lexer_get_token(&lexer))
 	{
 		stb_c_lexer_get_location(&lexer, lexer.where_firstchar, &loc);
-		printf("%3d:%3d : %.*s\n", loc.line_number, loc.line_offset, (int)(lexer.parse_point - lexer.where_firstchar), lexer.where_firstchar);
+		//printf("%3d:%3d : %.*s\n", loc.line_number, loc.line_offset, (int)(lexer.parse_point - lexer.where_firstchar), lexer.where_firstchar);
 	}
 
 	free(str.buf);
@@ -49,13 +50,14 @@ static void print_tokens(const char* filename)
 
 int main(void)
 {
-	print_tokens("stb_c_lexer_implementation.c");
-	putchar('\n');
-	print_tokens("utils.h");
-	putchar('\n');
-	print_tokens("utils.c");
-	putchar('\n');
-	print_tokens("main.c");
-	putchar('\n');
-	print_tokens("stb_c_lexer.h");
+	print_tokens("stb_c_lexer_implementation.c", false);
+	print_tokens("stb_c_lexer_implementation.c", true);
+	print_tokens("utils.h", false);
+	print_tokens("utils.h", true);
+	print_tokens("utils.c", false);
+	print_tokens("utils.c", true);
+	print_tokens("main.c", false);
+	print_tokens("main.c", true);
+	print_tokens("stb_c_lexer.h", false);
+	print_tokens("stb_c_lexer.h", true);
 }
